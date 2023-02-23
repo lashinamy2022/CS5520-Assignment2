391d9d22d468b835b08cca541deb5250aac37fa8
lashinamy@gmail.com <yangyang@yangyangdeMacBook-Air.local>
2023-02-22 20:44:36 -0800

    completed the features: 1.alert showing 2.checkmark button showing based on calories and reviewed fields 3.delete function 4.review function

--- screens/EditEntry.js
+++ screens/EditEntry.js
@@ -1,15 +1,14 @@
-import { StyleSheet, View, Text, Pressable } from "react-native";
+import { StyleSheet, View, Text, Pressable, Alert } from "react-native";
 import React from "react";
 import Card from "../components/Card";
 import CommonStyles from "../styles/CommonStyles";
 import Label from "../components/Label";
 import PressableArea from "../components/PressableArea";
 import { Ionicons } from "@expo/vector-icons";
-
-const EditEntry = () => {
+import { deleteDocById, updateDocById} from "../firebase/firebase-helper";
+const EditEntry = ({ navigation, route }) => {
   return (
     <View style={[CommonStyles.container, { alignItems: "center" }]}>
-
       <Card
         customizedStyle={[
           CommonStyles.purpleLight,
@@ -18,11 +17,14 @@ const EditEntry = () => {
       >
         <View style={[CommonStyles.center, { marginTop: 15 }]}>
           <Label
-            content="Calories: 560"
-            customizedStyle={[CommonStyles.fontSizeLarge, CommonStyles.fontPurpleDark]}
+            content={"Calories: " + route.params.entry.calories}
+            customizedStyle={[
+              CommonStyles.fontSizeLarge,
+              CommonStyles.fontPurpleDark,
+            ]}
           />
           <Label
-            content="Description: Breakfast"
+            content={"Description: " + route.params.entry.desc}
             customizedStyle={{ color: "rgb(60,61,132)", fontSize: 20 }}
           />
         </View>
@@ -33,12 +35,41 @@ const EditEntry = () => {
             { justifyContent: "space-evenly" },
           ]}
         >
-          <PressableArea customizedStyle={styles.pressableAreaCustom}>
+          <PressableArea
+            customizedStyle={styles.pressableAreaCustom}
+            areaPressed={() => {
+              Alert.alert("Delete", "Are you sure you want to delete this?", [
+                { text: "NO", onPress: () => console.log("No Pressed") },
+                {
+                  text: "YES",
+                  onPress: () => {
+                    deleteDocById(route.params.entry.id);
+                    navigation.navigate(route.params.pageName);
+                  },
+                },
+              ]);
+            }}
+          >
             <Ionicons name="trash-outline" size={20} color="#fff"></Ionicons>
           </PressableArea>
-          <PressableArea customizedStyle={styles.pressableAreaCustom}>
-            <Ionicons name="checkmark" size={20} color="#fff"></Ionicons>
-          </PressableArea>
+
+          {route.params.entry.calories > 500 && !route.params.entry.reviewed && (
+            <PressableArea customizedStyle={styles.pressableAreaCustom}
+            areaPressed={()=>{
+              Alert.alert("Important", "Are you sure you want to mark this item as reviewed?", [
+                { text: "NO", onPress: () => console.log("No Pressed") },
+                {
+                  text: "YES",
+                  onPress: () => {
+                    updateDocById(route.params.entry.id, {reviewed: true});
+                    navigation.navigate(route.params.pageName);
+                  },
+                },
+              ]);
+            }}>
+              <Ionicons name="checkmark" size={20} color="#fff"></Ionicons>
+            </PressableArea>
+          )}
         </View>
       </Card>
     </View>
