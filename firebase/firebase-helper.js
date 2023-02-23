1f0637a88f9a16d0a856df985b86cd27f920a30a
lashinamy@gmail.com <yangyang@yangyangdeMacBook-Air.local>
2023-02-22 20:35:05 -0800

    add delete function and update function

--- firebase/firebase-helper.js
+++ firebase/firebase-helper.js
@@ -5,7 +5,9 @@ import {
   query,
   where,
   onSnapshot,
+  updateDoc,
   deleteDoc,
+  serverTimestamp
 } from "firebase/firestore";
 import { firestore } from "./firebase-setup";
 
@@ -15,6 +17,22 @@ export async function writeToDB(entry) {
   try {
     const docRef = await addDoc(collection(firestore, "entries"), entry);
     console.log("Document written with ID: ", docRef.id);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function deleteDocById(deletedId) {
+  try {
+    await deleteDoc(doc(firestore, "entries", deletedId));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export async function updateDocById(updatedId, updatedData) {
+  try {
+    await updateDoc(doc(firestore, "entries", updatedId), {...updatedData, timestamp: serverTimestamp()});
   } catch (err) {
     console.log(err);
   }
